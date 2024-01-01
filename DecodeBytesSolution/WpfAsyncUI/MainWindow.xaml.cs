2c2ea4e75972103b6dc69f9f8428336ce7d0bdd3
Tural Suleymani <suleymani.tural@bp.com>
2024-01-01 16:35:48 +0000

    initial skeleton for async UI

---
+++ DecodeBytesSolution/WpfAsyncUI/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.Net.Http;
+using System.Windows;
+using System.Windows.Documents;
+using WpfAsyncUI.Services;
+
+namespace WpfAsyncUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly WebDocumentService _documentService;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _documentService = new WebDocumentService();
+        }
+
+        private async void load_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Operation started. Normal flow");
+            string contentUrl = "https://www.c-sharpcorner.com/article/microservices-vs-monoliths/";
+            var articleContent = await _documentService.GetContentAsync(contentUrl);
+            txbx_content.Document = AsFlowDocument(articleContent);
+        }
+
+        //private void load_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Debug.WriteLine("Operation started. Deadlock");
+        //    string contentUrl = "https://www.c-sharpcorner.com/article/microservices-vs-monoliths/";
+        //    var documentTask = _documentService.GetContentAsync(contentUrl);//schedule
+        //    documentTask.Wait();//block the UI
+        //    txbx_content.Document = AsFlowDocument("You are not going to see this message");
+        //}
+
+        private static FlowDocument AsFlowDocument(string content)
+        {
+            FlowDocument flowDocument = new();
+            flowDocument.Blocks.Add(new Paragraph(new Run(content)));
+            return flowDocument;
+        }
+
+    }
+}
---
+++ DecodeBytesSolution/WpfAsyncUI/Services/WebDocumentService.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http;
+
+namespace WpfAsyncUI.Services
+{
+    internal class WebDocumentService
+    {
+        public async Task<string> GetContentAsync(string requestUri)
+        {
+            return await new HttpClient().GetStringAsync(requestUri);
+        }
+    }
+}
