0eb0bbf8ed12277826dea167c2b4a56c6d7d4c7c
Tural Suleymani <suleymani.tural@bp.com>
2024-03-07 16:00:37 +0000

    transaction service added

---
+++ DecodeBytesSolution/ResultPattern/Service.Abstaction/ITransactionService.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Abstaction
+{
+    public interface ITransactionService
+    {
+        Task<bool> ProcessAsync(Account account);
+    }
+}
---
+++ DecodeBytesSolution/ResultPattern/Service.Core/TransactionService.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Models;
+using Service.Abstaction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Core
+{
+    internal class TransactionService : ITransactionService
+    {
+        private readonly IAccountServiceV2 _accountService;
+        public TransactionService(IAccountServiceV2 accountService)
+        {
+            _accountService = accountService;
+        }
+        public async Task<bool> ProcessAsync(Account account)
+        {
+            throw new NotImplementedException();
+            //var accountResponse = await _accountService.AddAsync(account);
+            //if (accountResponse.IsSuccess)
+            //{
+            //    Account MyAccount = accountResponse.Value;
+            //}
+
+            //if (accountResponse.IsFailure)
+            //{
+            //    string errorMessage = accountResponse.Error.ErrorMessage;
+            //}
+        }
+    }
+}
