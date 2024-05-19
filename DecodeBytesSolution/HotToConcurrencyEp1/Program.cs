e4c25ce9fb247e3bcfb5857329eb4f026bd48e4e
Tural Suleymani <suleymani.tural@bp.com>
2024-05-20 00:31:53 +0400

    how to ep1

---
+++ DecodeBytesSolution/HotToConcurrencyEp1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace HotToConcurrencyEp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The how to of Concurrency in C#. Ep1");
+            //captured variables
+            //string str = "Tural";
+
+            //Action act=()=>Console.WriteLine(str);
+
+            //str = "Change name";
+
+            //act();
+
+            for(int i = 0; i < 10; i++)
+            {
+                int temp = i;
+                new Thread(() =>
+                {
+                    Console.Write(temp + " ");
+                }).Start();
+            }
+            
+            Console.ReadLine();
+
+        }
+    }
+}
