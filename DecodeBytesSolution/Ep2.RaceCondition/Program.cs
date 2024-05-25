ba871f57338c31ff1629d913070fcb8adb79ab13
Tural Suleymani <suleymani.tural@bp.com>
2024-05-25 10:52:21 +0400

    initial commit

---
+++ DecodeBytesSolution/Ep2.RaceCondition/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Ep2.RaceCondition
+{
+    public class Transaction
+    {
+        public bool IsDone { get; private set; } = false;//shared resource
+        private static readonly object _locker = new object();
+        public void Transfer(decimal amount)
+        {
+            lock (_locker)
+            {
+                if (!IsDone)
+                {
+                    Console.WriteLine($"Transfer process started {Environment.CurrentManagedThreadId}");
+                    TransferMoney(amount);
+                    Console.WriteLine($"Transfer process ended {Environment.CurrentManagedThreadId}");
+                    IsDone = true;
+                }
+            }
+            
+        }
+
+        private void TransferMoney(decimal amount)
+        {
+            Console.WriteLine($"Transfering {amount}....");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Transaction t = new Transaction();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    t.Transfer(4000);
+                });
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
