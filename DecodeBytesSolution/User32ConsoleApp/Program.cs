037ab695f8e26e40323207de2f1f2cb66abf4b77
Tural Suleymani <suleymani.tural@bp.com>
2024-07-30 12:50:40 +0400

    dll integration

---
+++ DecodeBytesSolution/User32ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+
+namespace User32ConsoleApp
+{
+    internal class Program
+    {
+
+        [DllImport("file.dll")]
+        private static extern int multiply(int a, int b);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern int MessageBox(IntPtr owner, string message, string caption, uint type);
+        //MessageBox
+        static void Main(string[] args)
+        {
+            const int MS_OK = 0;
+            const int MS_OKCANCEL = 1;
+            const int MS_STOP = 16;
+            //MessageBox(IntPtr.Zero, "Hello from user32 dll", "Simple message", MS_STOP);
+
+            Console.WriteLine(multiply(4, 5));
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
